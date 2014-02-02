276388eb92497bb4fbf5d7aa1b879ed32fe4389d
unknown <John@John-PC.(none)>
2014-02-02 15:52:18 +0000

    treatise pages started

---
+++ scripts/library.js
@@ -0,0 +1,66 @@
+introhtml = "<div class='title'>";
+introhtml += "<br>Introduction";
+introhtml +="</div>";
+introhtml += "<div>";
+introhtml +="<br>Should you have read my story 'The Deceit of Numbers; then you will know that numbers are huge swirling bodies of varying shades and tones of red lava and blue ice that are moved around by red fire dragons and blue ice dragons in a realm protected by Unan, Chief of all gods. You will have also found out that on occasions when the correct red and blue numbers are combined then they destroy each other totally in flame and steam until nothing remains. If you have not yet read this story then I suggest it is enlightening to do so.";
+introhtml +="Understanding that numbers, as we use them, are mere representation of actual physical objects from the Domain of Numbers can help you understand the reasons for the rules that are used in Mathematics.";
+introhtml +="</div>";
+introhtml += "<div>";
+introhtml += "<br><input id='next' type='button' class='next' value='Next'><label id='nextLabel' for='next'>What is One?</label>";
+introhtml += "</div>";
+
+Intro={
+	page:"Introduction.html",
+	loaded:true,
+	html:introhtml,
+	next:null,
+	prev:null,
+	text:"Introduction",
+	links:null
+};
+
+One={
+	page:"One.html",
+	loaded:false,
+	html:"",
+	next:null,
+	prev:null,
+	text:"What is One?",
+	links:null
+};
+
+MinusOne={
+	page:"MinusOne.html",
+	loaded:false,
+	html:"",
+	next:null,
+	prev:null,
+	text:"What is Minus One",
+	links:null
+};
+
+Actions={
+	page:"Actions.html",
+	loaded:false,
+	html:"",
+	next:null,
+	prev:null,
+	text:"Actions and Results",
+	links:null
+};
+
+//NEXT
+Intro.next=One;
+One.next=MinusOne;
+
+//PREV
+One.prev=Intro;
+MinusOne.prev=One;
+
+//LINKS
+One.links=[Actions];
+MinusOne.links=[Actions];
+
+
+
+
---
+++ scripts/pageloader.js
@@ -0,0 +1,31 @@
+function loadPage(book,current,link) {	
+	var xhr = new XMLHttpRequest();
+	xhr.open("GET", "pages/"+book.page);
+	xhr.onreadystatechange=function() {
+		if (xhr.readyState==4) {
+	   		book.html=xhr.responseText;
+	   		book.loaded=true;
+	   		if(link) {	   			
+	   			var re = /prevLabel\" for=\"prev\">.*<\/label>/;
+				var ns ='prevLabel" for="prev">'+current.text+'</label>';
+console.log("in",book.html.match(re));				
+				book.html=book.html.replace(re,ns);			
+				book.prev=current;
+				current=book;
+				currentPage=book;
+	   			setPage();
+	   		}
+	   		if(current.next) {
+	   			document.getElementById("next").style.visibility="visible";
+	   			document.getElementById("nextLabel").style.visibility="visible";
+	   			document.getElementById("next").addEventListener('click', nextPage, false);
+	   		}
+	   		if(current.prev) {
+	   			document.getElementById("prev").style.visibility="visible";
+	   			document.getElementById("prevLabel").style.visibility="visible";
+	   			document.getElementById("prev").addEventListener('click', prevPage, false);
+	   		}
+	 	}
+	};
+	xhr.send(null);
+}
